801ebfe548edf4d0fa8db2838c498777c6560d58
RRFayad <renan.r.fayad@gmail.com>
2023-07-17 21:54:56 -0300

    sign up v0

--- src/shared/context/AuthContext.jsx
+++ src/shared/context/AuthContext.jsx
@@ -20,13 +20,17 @@ export function AuthContextProvider(props) {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState(null);
+  const [idToken, setIdToken] = useState(null);
+
+  const url = {
+    signUp: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    login: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    changePassword: `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  };
 
   useEffect(() => {
     localStorage.getItem('userId') ? setIsLoggedIn(true) : setIsLoggedIn(false);
-    setUserData(
-      DUMMY_USERS.find((user) => user.id === localStorage.getItem('userId')) ||
-        null
-    );
+    setUserData(DUMMY_USERS.find((user) => user.id === 'U0001'));
   });
 
   const loginHandler = (data) => {
@@ -55,14 +59,37 @@ export function AuthContextProvider(props) {
     return console.log(profileData);
   };
 
-  const signUpHandler = (data) => {
-    const newUserData = formHookDataMapper(data);
+  const signUpHandler = async (formData) => {
+    const newUserData = formHookDataMapper(formData);
 
-    localStorage.setItem('isLoggedIn', 'true');
-    setIsLoggedIn(true);
-    setUserData(DUMMY_USERS.find((user) => user.id === 'U0001'));
-    localStorage.setItem('userId', 'U0001');
-    return console.log(newUserData);
+    const response = await fetch(url.signUp, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: newUserData.email,
+        password: newUserData.password,
+        returnSecureToken: true,
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      const fetchedData = await response.json();
+      console.log(fetchedData);
+      fetchedData.localId &&
+        setUserData({ ...newUserData, id: fetchedData.localId });
+      fetchedData.idToken && setIdToken(fetchedData.idToken);
+      setIsLoggedIn(true);
+      localStorage.setItem('isLoggedIn', JSON.stringify(true));
+      localStorage.setItem('userId', JSON.stringify(fetchedData.localId));
+      localStorage.setItem('idToken', JSON.stringify(fetchedData.idToken));
+      console.log('User Signed Up Successfully!');
+    }
+    if (!response.ok) {
+      const errorData = await response.json();
+      const errorMessage = errorData.error.message
+        ? errorData.error.message
+        : 'Authentication Failed';
+      alert(errorMessage);
+    }
   };
 
   return (
